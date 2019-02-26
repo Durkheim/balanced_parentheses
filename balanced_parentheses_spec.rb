d91c6803758f1ca2ca4d132ef501675e5f429c86
Durkheim <mosesyk.dev.bootcamp@gmail.com>
2019-02-26 17:12:26 -0600

    update language in rspec

--- balanced_parentheses_spec.rb
+++ balanced_parentheses_spec.rb
@@ -4,7 +4,7 @@ describe BalancedParentheses do
 
   describe "#balanced?" do
 
-    context "when string contains balanced parentheses" do
+    context "when a string contains balanced parentheses" do
       it "should return true" do
         expect(BalancedParentheses.new("()").balanced?).to be true
         expect(BalancedParentheses.new("(()())").balanced?).to be true
@@ -15,7 +15,7 @@ describe BalancedParentheses do
     end
 
 
-    context "when string contains unbalanced parentheses" do
+    context "when a string contains unbalanced parentheses" do
       it "should return false" do
         expect(BalancedParentheses.new(")(").balanced?).to be false
         expect(BalancedParentheses.new("())").balanced?).to be false
