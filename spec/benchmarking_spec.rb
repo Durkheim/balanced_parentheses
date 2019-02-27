278be28137c871203bbf308212e6b2a4c7cf901f
Durkheim <mosesyk.dev.bootcamp@gmail.com>
2019-02-27 11:38:10 -0600

    specify type of implementation in benchmark tests

--- spec/benchmarking_spec.rb
+++ spec/benchmarking_spec.rb
@@ -5,8 +5,8 @@ require_relative "sample_strings"
 require "benchmark"
 
 describe "Benchmarking#balanced?" do
-  let(:first_implementation) { BalancedParentheses }
-  let(:second_implementation) { BalancedParenthesesTwo }
+  let(:object_oriented_implementation) { BalancedParentheses }
+  let(:ledger_implementation) { BalancedParenthesesTwo }
   
 
   context "when comparing the performance of implementations" do
@@ -15,35 +15,35 @@ describe "Benchmarking#balanced?" do
     let(:sample_balanced_string) { BALANCED_STRINGS.sample }
     
 
-    let(:balanced_performance_of_first_implementation) { Benchmark.realtime {
-        first_implementation.new(:sample_balanced_string).balanced?
+    let(:balanced_performance_of_object_oriented_implementation) { Benchmark.realtime {
+        object_oriented_implementation.new(:sample_balanced_string).balanced?
       }
     }
 
-    let(:balanced_performance_of_second_implementation) { Benchmark.realtime {
-        second_implementation.new(:sample_balanced_string).balanced?
+    let(:balanced_performance_of_ledger_implementation) { Benchmark.realtime {
+        ledger_implementation.new(:sample_balanced_string).balanced?
       }
     }
 
     it "should be within 1 millisecond when evaluating a balanced string" do
-      expect(balanced_performance_of_second_implementation).to be_within(0.001).of(balanced_performance_of_first_implementation)
+      expect(balanced_performance_of_ledger_implementation).to be_within(0.001).of(balanced_performance_of_object_oriented_implementation)
     end
 
 
     let(:sample_unbalanced_string) { UNBALANCED_STRINGS.sample }
 
-    let(:unbalanced_performance_of_first_implementation) { Benchmark.realtime {
-        first_implementation.new(:sample_unbalanced_string).balanced?
+    let(:unbalanced_performance_of_object_oriented_implementation) { Benchmark.realtime {
+        object_oriented_implementation.new(:sample_unbalanced_string).balanced?
       }
     }
 
-    let(:unbalanced_performance_of_second_implementation) { Benchmark.realtime {
-        second_implementation.new(:sample_unbalanced_string).balanced?
+    let(:unbalanced_performance_of_ledger_implementation) { Benchmark.realtime {
+        ledger_implementation.new(:sample_unbalanced_string).balanced?
       }
     }
 
-    it "should be within 1 millisecond when evaluating a balanced string" do
-      expect(unbalanced_performance_of_second_implementation).to be_within(0.001).of(unbalanced_performance_of_first_implementation)
+    it "should be within 1 millisecond when evaluating an unbalanced string" do
+      expect(unbalanced_performance_of_ledger_implementation).to be_within(0.001).of(unbalanced_performance_of_object_oriented_implementation)
     end
   end  
 end
