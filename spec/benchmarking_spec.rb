512190cdee435f9b36711b04559166010380207a
Durkheim <mosesyk.dev.bootcamp@gmail.com>
2019-02-27 11:20:01 -0600

    updating performance test verbage

--- spec/benchmarking_spec.rb
+++ spec/benchmarking_spec.rb
@@ -9,41 +9,41 @@ describe "Benchmarking#balanced?" do
   let(:second_implementation) { BalancedParenthesesTwo }
   
 
-  context "when comparing the performance of the optimized solution against the unopitmized solution" do
+  context "when comparing the performance of implementations" do
 
 
     let(:sample_balanced_string) { BALANCED_STRINGS.sample }
     
 
-    let(:balanced_unoptimized_performance) { Benchmark.realtime {
+    let(:balanced_performance_of_first_implementation) { Benchmark.realtime {
         first_implementation.new(:sample_balanced_string).balanced?
       }
     }
 
-    let(:balanced_optimized_performance) { Benchmark.realtime {
+    let(:balanced_performance_of_second_implementation) { Benchmark.realtime {
         second_implementation.new(:sample_balanced_string).balanced?
       }
     }
 
     it "should be within 1 millisecond when evaluating a balanced string" do
-      expect(balanced_optimized_performance).to be_within(0.001).of(balanced_unoptimized_performance)
+      expect(balanced_performance_of_second_implementation).to be_within(0.001).of(balanced_performance_of_first_implementation)
     end
 
 
     let(:sample_unbalanced_string) { UNBALANCED_STRINGS.sample }
 
-    let(:unbalanced_unoptimized_performance) { Benchmark.realtime {
+    let(:unbalanced_performance_of_first_implementation) { Benchmark.realtime {
         first_implementation.new(:sample_unbalanced_string).balanced?
       }
     }
 
-    let(:unbalanced_optimized_performance) { Benchmark.realtime {
+    let(:unbalanced_performance_of_second_implementation) { Benchmark.realtime {
         second_implementation.new(:sample_unbalanced_string).balanced?
       }
     }
 
     it "should be within 1 millisecond when evaluating a balanced string" do
-      expect(unbalanced_optimized_performance).to be_within(0.001).of(unbalanced_unoptimized_performance)
+      expect(unbalanced_performance_of_second_implementation).to be_within(0.001).of(unbalanced_performance_of_first_implementation)
     end
   end  
 end
